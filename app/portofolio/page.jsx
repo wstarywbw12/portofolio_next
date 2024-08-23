8a10995d33c9f3d449543a55d561298c235a2bd3
WistuAryW <wistuariwibowo6@gmail.com>
2024-08-23 20:34:13 +0700

    add work

--- app/portofolio/page.jsx
+++ app/portofolio/page.jsx
@@ -1,168 +1,70 @@
 "use client";
 
-import { FaBootstrap, FaPhp, FaLaravel, FaJs } from "react-icons/fa";
-
-import { useState } from "react";
 import { motion } from "framer-motion";
 import "swiper/css";
 
 const projects = [
   {
-    num: "01",
+    id: "01",
     category: "Web Development",
     title: "WEB SISTEM PAKAR",
     description:
       "Aplikasi Sistem Pakar Diagnosa OPT Semangka adalah aplikasi untuk membantu petani dalam mengidentifikasi OPT yang menyerang tanaman semangka. Memanfaatkan teknologi kecerdasan buatan, web ini mampu memberikan diagnosa yang akurat.",
-    stack: [
-      {
-        name: "Bootstrap",
-        icon: <FaBootstrap />,
-      },
-      {
-        name: "JavaScript",
-        icon: <FaJs />,
-      },
-      {
-        name: "PHP",
-        icon: <FaPhp />,
-      },
-      {
-        name: "Laravel",
-        icon: <FaLaravel />,
-      },
-    ],
     image: "/assets/work/thumb1.png",
-    live: "https://github.com",
-    github: "https://github.com",
     link: "sistempakar.id",
   },
   {
-    num: "02",
+    id: "02",
+    category: "Web Development",
+    title: "WEB Order Kuliner",
+    description:
+      "Web Order Kuliner adalah layanan pesan antar makanan online yang memungkinkan pengguna untuk memesan makanan dari berbagai restoran melalui internet. Biasanya, pengguna dapat mencari restoran berdasarkan lokasi, jenis makanan, atau rating. ",
+    image: "/assets/work/thumb5.png",
+    link: "orderfood.id",
+  },
+  {
+    id: "03",
     category: "Web Development",
     title: "WEB PORTAL BERITA",
     description:
       "Web Portal Berita adalah platform berita daring yang dirancang untuk memberikan informasi terkini dan terpercaya kepada pengguna. Situs ini menyediakan berbagai kategori berita mulai dari politik, ekonomi, teknologi dengan tampilan yang bersih dan user-friendly.",
-    stack: [
-      {
-        name: "Bootstrap",
-        icon: <FaBootstrap />,
-      },
-      {
-        name: "JavaScript",
-        icon: <FaJs />,
-      },
-      {
-        name: "PHP",
-        icon: <FaPhp />,
-      },
-      {
-        name: "Laravel",
-        icon: <FaLaravel />,
-      },
-    ],
     image: "/assets/work/thumb2.png",
-    live: "https://github.com",
-    github: "https://github.com",
     link: "portalberita.id",
   },
   {
-    num: "03",
+    id: "04",
     category: "Web Development",
     title: "WEB ABSENS QR CODE",
     description:
       "Aplikasi Web Absensi Sekolah dengan QR Code adalah solusi inovatif untuk mempermudah proses absensi siswa di sekolah. Dengan memanfaatkan teknologi QR Code, aplikasi ini memungkinkan guru dan siswa untuk melakukan absensi dengan cepat dan akurat.",
-    stack: [
-      {
-        name: "Bootstrap",
-        icon: <FaBootstrap />,
-      },
-      {
-        name: "JavaScript",
-        icon: <FaJs />,
-      },
-      {
-        name: "PHP",
-        icon: <FaPhp />,
-      },
-      {
-        name: "Laravel",
-        icon: <FaLaravel />,
-      },
-    ],
     image: "/assets/work/thumb3.png",
-    live: "https://github.com",
-    github: "https://github.com",
     link: "absenqrcode.id",
   },
   {
-    num: "03",
+    id: "05",
     category: "Web Development",
     title: "WEB UJIAN CBT SEKOLAH",
     description:
       "Aplikasi Web Ujian CBT Sekolah adalah solusi inovatif yang dirancang untuk menyederhanakan dan meningkatkan efisiensi proses ujian di sekolah. Dengan menggunakan teknologi berbasis komputer, aplikasi ini menggantikan ujian berbasis kertas tradisional.",
-    stack: [
-      {
-        name: "Bootstrap",
-        icon: <FaBootstrap />,
-      },
-      {
-        name: "JavaScript",
-        icon: <FaJs />,
-      },
-      {
-        name: "PHP",
-        icon: <FaPhp />,
-      },
-      {
-        name: "Laravel",
-        icon: <FaLaravel />,
-      },
-    ],
     image: "/assets/work/thumb4.png",
-    live: "https://github.com",
-    github: "https://github.com",
     link: "ujiancbt.id",
   },
 ];
 
 const Work = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedProject, setSelectedProject] = useState(null);
-
-  const openModal = (project) => {
-    setSelectedProject(project);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedProject(null);
-  };
-
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: {
-          delay: 2.4,
-          duration: 0.4,
-          ease: "easeIn",
-        },
-      }}
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
         <h2 className="text-center text-4xl font-bold text-white mt-4 mb-2 md:mb-10">
           My Projects
         </h2>
-
         <div className="mt-16 grid grid-cols-1 lg:grid-cols-2 gap-8 mb-5">
           {projects.map((project) => (
-            <div
-              key={project.num}
-              className="card flex flex-col gap-4 p-4 group"
-            >
+            <div key={project.id} className="card flex flex-col gap-4 p-4 group">
               <div className="card-img transition-all duration-500">
                 <img
                   src={project.image}
@@ -175,18 +77,13 @@ const Work = () => {
               <div className="h-full pt-8 px-4">
                 <h1 className="text-2xl font-bold mb-4 text-start">{project.title}</h1>
                 <p className="text-slate-500">{project.description}</p>
-
                 <div className="flex flex-wrap gap-2 mt-8">
-                  <img src="https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark" />
-                  <img src="https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark" />
+                  <img src="https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark" alt="Laravel Badge" />
+                  <img src="https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark" alt="Bootstrap Badge" />
                 </div>
               </div>
               <div className="mb-4">
-                <a
-                  href={project.link}
-                  target="_blank"
-                  className="card-link"
-                >
+                <a href={`https://${project.link}`} target="_blank" rel="noopener noreferrer" className="card-link">
                   <p>Website</p>
                   <p className="text-teal-400">{project.link}</p>
                 </a>
