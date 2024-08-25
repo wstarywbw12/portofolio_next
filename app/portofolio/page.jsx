7d716ffc8b01dab7b3bfe94386bb9f0c2ddf5351
WistuAryW <wistuariwibowo6@gmail.com>
2024-08-25 10:24:48 +0700

    add work

--- app/portofolio/page.jsx
+++ app/portofolio/page.jsx
@@ -7,46 +7,79 @@ const projects = [
   {
     id: "01",
     category: "Web Development",
+    title: "WEB Spotify Clone",
+    description:
+      "Spotify Clone is a web-based music streaming platform designed to mimic the core functionalities of the popular Spotify service. It offers users a seamless experience to discover, listen to, and manage their favorite music.",
+    image: "/assets/work/thumb6.png",
+    techStack: [
+      "https://svgl-badge.vercel.app/api/Library/React?theme=dark",
+      "https://svgl-badge.vercel.app/api/Framework/Tailwind%20CSS?theme=dark",
+    ],
+    link: "spotifyclone.id",
+  },
+  {
+    id: "02",
+    category: "Web Development",
     title: "WEB SISTEM PAKAR",
     description:
       "Aplikasi Sistem Pakar Diagnosa OPT Semangka adalah aplikasi untuk membantu petani dalam mengidentifikasi OPT yang menyerang tanaman semangka. Memanfaatkan teknologi kecerdasan buatan, web ini mampu memberikan diagnosa yang akurat.",
     image: "/assets/work/thumb1.png",
+    techStack: [
+      "https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark",
+      "https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark",
+    ],
     link: "sistempakar.id",
   },
   {
-    id: "02",
+    id: "03",
     category: "Web Development",
     title: "WEB Order Kuliner",
     description:
       "Web Order Kuliner adalah layanan pesan antar makanan online yang memungkinkan pengguna untuk memesan makanan dari berbagai restoran melalui internet. Biasanya, pengguna dapat mencari restoran berdasarkan lokasi, jenis makanan, atau rating. ",
     image: "/assets/work/thumb5.png",
+    techStack: [
+      "https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark",
+      "https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark",
+    ],
     link: "orderfood.id",
   },
   {
-    id: "03",
+    id: "04",
     category: "Web Development",
     title: "WEB PORTAL BERITA",
     description:
       "Web Portal Berita adalah platform berita daring yang dirancang untuk memberikan informasi terkini dan terpercaya kepada pengguna. Situs ini menyediakan berbagai kategori berita mulai dari politik, ekonomi, teknologi dengan tampilan yang bersih dan user-friendly.",
     image: "/assets/work/thumb2.png",
+    techStack: [
+      "https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark",
+      "https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark",
+    ],
     link: "portalberita.id",
   },
   {
-    id: "04",
+    id: "05",
     category: "Web Development",
     title: "WEB ABSENS QR CODE",
     description:
       "Aplikasi Web Absensi Sekolah dengan QR Code adalah solusi inovatif untuk mempermudah proses absensi siswa di sekolah. Dengan memanfaatkan teknologi QR Code, aplikasi ini memungkinkan guru dan siswa untuk melakukan absensi dengan cepat dan akurat.",
     image: "/assets/work/thumb3.png",
+    techStack: [
+      "https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark",
+      "https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark",
+    ],
     link: "absenqrcode.id",
   },
   {
-    id: "05",
+    id: "06",
     category: "Web Development",
     title: "WEB UJIAN CBT SEKOLAH",
     description:
       "Aplikasi Web Ujian CBT Sekolah adalah solusi inovatif yang dirancang untuk menyederhanakan dan meningkatkan efisiensi proses ujian di sekolah. Dengan menggunakan teknologi berbasis komputer, aplikasi ini menggantikan ujian berbasis kertas tradisional.",
     image: "/assets/work/thumb4.png",
+    techStack: [
+      "https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark",
+      "https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark",
+    ],
     link: "ujiancbt.id",
   },
 ];
@@ -78,8 +111,9 @@ const Work = () => {
                 <h1 className="text-2xl font-bold mb-4 text-start">{project.title}</h1>
                 <p className="text-slate-500">{project.description}</p>
                 <div className="flex flex-wrap gap-2 mt-8">
-                  <img src="https://svgl-badge.vercel.app/api/Framework/Laravel?theme=dark" alt="Laravel Badge" />
-                  <img src="https://svgl-badge.vercel.app/api/Framework/Bootstrap?theme=dark" alt="Bootstrap Badge" />
+                  {project.techStack.map((tech, index) => (
+                    <img key={index} src={tech} alt={`Tech stack ${index}`} />
+                  ))}
                 </div>
               </div>
               <div className="mb-4">
